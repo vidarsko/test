0c4f8a97ffdb9de804a80216fdca0da0a9ffc9f9
Vidar <vidarsko@gmail.com>
2014-08-21 14:21:43 +0200

    Hallo

---
+++ one.cpp
@@ -0,0 +1,14 @@
+// my first program in cpp
+
+/* This is a block comment
+   and have no impact on th program what so ever */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\n";
+  cout << "Jeg er et c++ - program\n";
+  cout << "Halloooo" << endl
+}
